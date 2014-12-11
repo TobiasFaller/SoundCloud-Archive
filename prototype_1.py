d9e8a8e78c58f894594ecb2226c15d50c5f3477d
Alex Crisara <crisara.a@husky.neu.edu>
2014-12-11 18:55:14 -0500

    Uploaded Initial Files
    Uploaded Virtualenv for script
    Uploaded test file

---
+++ prototype_1.py
@@ -0,0 +1,27 @@
+#Test program created by Alex Crisara on Sun 10.26.2014 @ 8:53 pm in Davenport Commons A
+#The only purpose of this is to test Soundcloud API things...
+#NOTE when installing things in virtualenv USE SUDO WITH PIP
+
+import soundcloud
+i = 1
+
+# create client object with app and user credentials
+client = soundcloud.Client(client_id='491887981f67eb79c3c7cefbeccfebf4',
+                           client_secret='cc19c241769e2284da6a377452fce946',
+                           username='oztrance',
+                           password='soundoftheOZ13')
+
+# print authenticated user's username
+print "+---------------------+"
+print "Active User - %s" %client.get('/me').username  #YUSS THIS WORKED
+print "+---------------------+"
+# get playlist
+#so basically since the API already knows who you are just put '/playlists' before the name of the playlist
+playlist = client.get('/playlists/deep')
+
+# list tracks in playlist
+for track in playlist.tracks:
+    a = "%d - " %i 
+    b = track['title']
+    print a + b
+    i = i + 1
