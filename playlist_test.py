1d373f7ff5197becd500aa7d8ad314b11b9b89d5
Alex Crisara <crisara.a@husky.neu.edu>
2015-02-23 17:02:47 -0500

    Added permalink url functionality

--- playlist_test.py
+++ playlist_test.py
@@ -2,14 +2,17 @@
 
 import requests, os.path, soundcloud
 
+#encapsulated function for future use
 def enumerate_plsts(client_id,base_url):
 	#list all playlists for given user
 	client = soundcloud.Client(client_id=client_id)
 	user = client.get('/resolve', url=base_url)
 	playlists = client.get('/users/%d/playlists' %user.id)
-	print playlists
+	
+	for i in range(0,len(playlists)):
+		print('%d - %s' %(i, playlists[i].title))
 
-
+#soundcloud API info
 client_id = '9fbbd1e3baad458473e7cf3f9334f43c'
 base_url = 'https://soundcloud.com/oztrance/'
 
@@ -17,5 +20,13 @@ client = soundcloud.Client(client_id=client_id)
 user = client.get('/resolve', url=base_url)
 playlists = client.get('/users/%d/playlists' %user.id)
 
+print '+------------------------------------------+'
+print '| Raw Title: |'
 for i in range(0,len(playlists)):
 	print('%d - %s' %(i, playlists[i].title))
+print '+------------------------------------------+'
+
+print '| Raw URL |'
+for i in range(0,len(playlists)):
+	print('%d - %s' %(i, playlists[i].permalink_url))
+print '+------------------------------------------+'
