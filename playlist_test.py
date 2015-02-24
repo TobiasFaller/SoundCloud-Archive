296727a7e62bd38ae123c1fc2ebde64320fb55ad
Alex Crisara <crisara.a@husky.neu.edu>
2015-02-23 22:50:26 -0500

    the track counter works now

--- playlist_test.py
+++ playlist_test.py
@@ -12,6 +12,14 @@ def enumerate_plsts(client_id,base_url):
 	for i in range(0,len(playlists)):
 		print('%d - %s' %(i, playlists[i].title))
 
+
+def count_tracks(playlist):
+	num_tracks = 0
+	for track in playlist.tracks:
+		num_tracks = num_tracks + 1
+	return num_tracks
+
+
 #soundcloud API info
 client_id = '9fbbd1e3baad458473e7cf3f9334f43c'
 base_url = 'https://soundcloud.com/oztrance/'
@@ -20,13 +28,21 @@ client = soundcloud.Client(client_id=client_id)
 user = client.get('/resolve', url=base_url)
 playlists = client.get('/users/%d/playlists' %user.id)
 
+#enumerate playlist titles
 print '+------------------------------------------+'
-print '| Raw Title: |'
+print '| Title: |'
 for i in range(0,len(playlists)):
 	print('%d - %s' %(i, playlists[i].title))
 print '+------------------------------------------+'
 
-print '| Raw URL |'
+#enumerate playlist root url's
+print '| URL |'
 for i in range(0,len(playlists)):
 	print('%d - %s' %(i, playlists[i].permalink_url))
+print '+------------------------------------------+'
+
+#enumerate tracks in playlists
+print '| # Tracks |'
+for i in range(0,len(playlists)):
+	print('%d - %s tracks') %(i, count_tracks(playlists[i]))
 print '+------------------------------------------+'
